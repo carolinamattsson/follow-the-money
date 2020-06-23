126c1b6f179723db1572624be438f08587d981ed
Carolina Mattsson <mattsson.c@husky.neu.edu>
2020-06-23 12:06:05 -0400

    finished example 3 notes

--- examples/3_time_window/script.sh
+++ examples/3_time_window/script.sh
@@ -2,5 +2,5 @@
 
 # Limiting the size of tracked flows
 python3 ../../follow_the_money.py account_txns+.csv account_config.json output --prefix prev_ --lifo --mixed
-python3 ../../follow_the_money.py account_txns+.csv account_config+.json output --prefix plus_ --lifo --mixed
 python3 ../../follow_the_money.py account_txns+.csv account_config-.json output --prefix minus_ --lifo --mixed
+python3 ../../follow_the_money.py account_txns+.csv account_config+.json output --prefix plus_ --lifo --mixed
