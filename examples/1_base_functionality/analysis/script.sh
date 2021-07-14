f1caf1793f233f09fedd87794769884b7cf6c58c
Carolina Mattsson <mattsson.c@northeastern.edu>
2021-07-14 18:19:12 +0200

    examples for users_savings.py

--- examples/1_base_functionality/analysis/script.sh
+++ examples/1_base_functionality/analysis/script.sh
@@ -4,3 +4,7 @@
 python3 ../../../analysis/motifs.py ../output/network_flows_lifo.csv ./ --prefix network_
 python3 ../../../analysis/motifs.py ../output/network_flows_lifo.csv ./ --prefix network_ --suffix _timewindow --timewindow "(2017-03-01 00:00:00,2017-03-02 00:00:00)"
 python3 ../../../analysis/motifs.py ../output/network_flows_lifo.csv ./ --prefix network_ --suffix _join --join "[check_deposit,direct_deposit,cash_deposit]" --name "deposit"
+
+# Base functionality -- users_savings.py
+python3 ../../../analysis/users_savings.py ../output/network_flows_lifo.csv ./ --prefix network_
+python3 ../../../analysis/users_savings.py ../output/network_flows_lifo.csv ./ --prefix network_ --suffix _timewindow --timewindow "(2017-03-01 00:00:00,2017-03-02 01:30:00)"
--- examples/2_size_limits/analysis/script.sh
+++ examples/2_size_limits/analysis/script.sh
@@ -2,4 +2,8 @@
 
 # "untracked" funds -- motifs.py
 python3 ../../../analysis/motifs.py ../../1_base_functionality/output/account_flows_mixed.csv ./ --prefix account_ --suffix _mixed
-python3 ../../../analysis/motifs.py ../output/size_flows_mixed.csv ./ --prefix size_ --suffix _mixed
+python3 ../../../analysis/motifs.py ../output/size_flows_mixed.csv ./ --prefix untracked_ --suffix _mixed
+
+# "untracked" funds -- users_savings.py
+python3 ../../../analysis/users_savings.py ../../1_base_functionality/output/account_flows_mixed.csv ./ --prefix account_ --suffix _mixed
+python3 ../../../analysis/users_savings.py ../output/size_flows_mixed.csv ./ --prefix untracked_ --suffix _mixed
